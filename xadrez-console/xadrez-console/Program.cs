65ea6d18b173b85975901a05666bf4b47748960f
Matheus Bueno Faria <106281139+matheusbuenowb@users.noreply.github.com>
2023-12-03 13:19:27 -0300

    Configurações iniciais
    Criação das classes tabuleiro, posição, peça, cor e tela.

---
+++ xadrez-console/xadrez-console/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using tabuleiro;
+
+namespace xadrez_console{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Posicao P;
+
+            P = new Posicao(3, 4);
+
+            Console.WriteLine("Posição: " + P);
+
+            Tabuleiro tab = new Tabuleiro(8,8);
+
+            Tela.imprimirTabuleiro(tab);
+        }
+    }
+
+    }
---
+++ xadrez-console/xadrez-console/tabuleiro/Tabuleiro.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tabuleiro
+{
+    internal class Tabuleiro
+    {
+        public int linhas { get; set; }
+        public int colunas { get; set; }
+
+        private Peca[,] pecas;
+        
+        public Tabuleiro (int linhas, int colunas)
+        {
+            this.linhas = linhas;
+            this.colunas = colunas;
+            pecas = new Peca[linhas, colunas];
+        }
+
+        public Peca peca(int linha, int coluna)
+        {
+            return pecas[linha, coluna];
+        }
+
+    }
+}
